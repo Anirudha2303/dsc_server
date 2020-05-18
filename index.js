4bcf0d760feb574377c1275645a3049693a3badc
Aryan Sisodia <shrn82256@gmail.com>
2020-05-18 15:55:21 +0530

    added nearby check for schools

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan("dev"));
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/speed", (req, res, next) => {
+app.get("/speed", async (req, res) => {
   const { latitude, longitude } = req.query;
 
   if (isNaN(latitude))
@@ -25,7 +25,34 @@ app.get("/speed", (req, res, next) => {
   if (isNaN(longitude))
     return res.status(400).json({ message: "Longitude is not a number" });
 
-  axios({
+  const searchRes = await axios({
+    method: "get",
+    baseURL: "https://atlas.microsoft.com/",
+    url: "/search/poi/category/json/",
+    params: {
+      "api-version": 1.0,
+      "subscription-key": process.env.AZURE_KEY,
+      query: "SCHOOL",
+      lat: latitude,
+      lon: longitude,
+      radius: 50,
+      limit: 3,
+    },
+  });
+
+  console.log(
+    "searchRes.data.summary.numResults: ",
+    searchRes.data.summary.numResults
+  );
+
+  if (searchRes.data.summary.numResults > 0) {
+    return res.status(200).json({
+      currentSpeed: 30,
+      freeFlowSpeed: 30,
+    });
+  }
+
+  const trafficRes = await axios({
     method: "get",
     baseURL: "https://atlas.microsoft.com/",
     url: "/traffic/flow/segment/json/",
@@ -35,30 +62,23 @@ app.get("/speed", (req, res, next) => {
       query: `${latitude},${longitude}`,
       style: "absolute",
       unit: "KMPH",
-      zoom: 10
-    }
-  })
-    .then(response => {
-      const {
-        currentSpeed,
-        freeFlowSpeed,
-        confidence,
-        roadClosure
-      } = response.data.flowSegmentData;
+      zoom: 10,
+    },
+  });
 
-      res.status(200).json({
-        currentSpeed,
-        freeFlowSpeed,
-        confidence,
-        roadClosure
-      });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(400).json({
-        message: err.message
-      });
-    });
+  const {
+    currentSpeed,
+    freeFlowSpeed,
+    confidence,
+    roadClosure,
+  } = trafficRes.data.flowSegmentData;
+
+  res.status(200).json({
+    currentSpeed,
+    freeFlowSpeed,
+    confidence,
+    roadClosure,
+  });
 });
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
